8bf97456815212eb0bdb67c4f2dbb41b04c3876d
hxnsahasan <141004963+hxnsahasan@users.noreply.github.com>
2023-08-07 03:44:36 +0700

    Add files via upload

---
+++ hasan.py
@@ -0,0 +1,123 @@
+#!/bin/python
+
+#module
+import os, sys, time
+
+#pembersih texs
+def bersih():
+    os.system("clear")
+
+# tampilan
+def menu():
+  bersih()
+  print("\033[1;33m")
+  os.system("figlet HASAN")
+  print("Author: HASAN")
+  print("                                    ")
+  print("\033[1;32m[\033[1;37m-\033[1;32m] \033[1;36mTools Created by HXNsA (Hasan)")
+  print("                                 ")
+  print("                                 ")
+  print("\033[1;31m[\033[1;37m::\033[1;31m] \033[1;33mPilih Daftar Menu \033[1;31m[\033[1;37m::\033[1;31m]")
+  print("                                    ")
+  print("                            ╔═══════════════════════╗")
+  print("                            ║ \033[1;33mPR \033[1;31m║    \033[1;37mPERKENALAN    \033[1;31m║")
+  print("                            ╚═══════════════════════╝")
+  print("                                        ║  ")
+  print("                            ╔═══════════════════════╗")
+  print("                            ║ \033[1;33mUP \033[1;31m║    \033[1;37mUPDATE        \033[1;31m║")
+  print("                            ╚═══════════════════════╝")
+  print("                                        ║")
+  print("                                        ║")
+  print("                                        ║")
+  print("                   ╔════════════════════║═════════════════════╗")
+  print("                   ║                    ║                     ║")
+  print("                   v                    v                     v")
+  print("                \033[1;33mSPAMING              \033[1;33mHACKING              \033[1;33m HACKING")
+  print("        \033[1;31m ╔══════════════════╗ ╔════════════════════╗ ╔══════════════════╗")
+  print("         ║ \033[1;33mS1 \033[1;31m║ \033[1;37mSPAM WA     \033[1;31m║ ║ \033[1;33mH1 \033[1;31m║ \033[1;37mHACK FB BRAYEN\033[1;31m║ ║ \033[1;33mH3 \033[1;31m║ \033[1;37mHACK IG     \033[1;31m║")
+  print("         ║ \033[1;33mS2 \033[1;31m║ \033[1;37mSPAM CALL WA\033[1;31m║ ║ \033[1;33mH2 \033[1;31m║ \033[1;37mHACK FB WTHAT \033[1;31m║ ║ \033[1;33mH4 \033[1;31m║ \033[1;37mHACK HASAN  \033[1;31m║")
+  print("         ╚══════════════════╝ ╚════════════════════╝ ╚══════════════════╝")
+  print("                                  ")
+  print("\033[1;31m[\033[1;37m-\033[1;31m] \033[1;33mPilih Opsi :")
+  print("         ")
+  print("\033[1;31m║")
+  contoh = input("\033[1;31m╚═══> \033[1;33m")
+  if contoh =="PR":
+    kenal=input("siapa nama kamu: ")
+    print("Hay "+ kenal)
+    print("                                     ")
+  if contoh =="UP":
+    os.system("pkg update")
+    os.system("pkg upgrade")
+  if contoh =="H1":
+    print("MENGINSTAL SCRIPT")
+    print("                            ")
+    os.system("pkg update && pkg upgrade")
+    os.system("pkg install python")
+    os.system("pkg install git")
+    os.system("pip install stdiomask")
+    os.system("pip install requests")
+    os.system("pip install bs4")
+    os.system("pip install requests")
+    os.system("pip install rich")
+    os.system("pip install mechanize")
+    os.system("git clone https://github.com/BrayennnXD/Brayennn3.4")
+    os.system("cd /storage/emulated/0/Brayennn3.4/")
+    os.system("python BrayennnFB.py")
+    print("                     ")
+    print("SELESAI MENGINSTAL SCRIPT")
+    print("KETIKAN $cd cd /storage/emulated/0/Brayennn3.4/ LALU ENTER")
+    print("KETIKAN LAGI $python BrayennnFB.py")
+    print("!!!KETIKAN LANGKAH DI ATAS TANPA TANPA TANDA $")
+  if contoh =="H2":
+    os.system("pkg update && pkg upgrade")
+    os.system("pkg install python")
+    os.system("pkg install git")
+    os.system("pip install stdiomask")
+    os.system("pip install requests")
+    os.system("pip install bs4")
+    os.system("pip install requests")
+    os.system("pip install rich")
+    os.system("pip install mechanize")
+    os.system("git clone https://github.com/hxnsahasan/mbasic/")
+    os.system("cd mbasic")
+    os.system("git pull")
+    os.system("python mbasic2.py")
+  if contoh =="H3":
+    os.system("pkg update && pkg upgrade")
+    os.system("pkg install python")
+    os.system("pkg install git")
+    os.system("pip install stdiomask")
+    os.system("pip install requests")
+    os.system("pip install bs4")
+    os.system("pip install requests")
+    os.system("pip install rich")
+    os.system("pip install mechanize")
+    os.system("git clone https://github.com/hxnsahasan/mbasic/")
+    os.system("cd mvrk")
+    os.system("git pull")
+    os.system("python mvrk.py")
+  if contoh =="H4":
+    os.system("pkg update && pkg upgrade")
+    os.system("pkg install python")
+    os.system("pkg install git")
+    os.system("pip install stdiomask")
+    os.system("pip install requests")
+    os.system("pip install bs4")
+    os.system("pip install requests")
+    os.system("pip install rich")
+    os.system("pip install mechanize")
+    os.system("git clone https://github.com/hxnsahasan/hxnsav2")
+    os.system("cd mbasic")
+    os.system("git pull")
+    os.system("python hxnsav2.py")
+menu ()
+
+
+green= '\033[1;32m'
+red= '\033[1;31m'
+yellow= '\033[1;33m'
+white= '\033[1;37m'
+blue= '\033[1;34m'
+purple= '\033[1;35m'
+cyan= '\033[1;36m'
